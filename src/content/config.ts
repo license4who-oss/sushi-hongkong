99f45cc06499d7ff1e0f44ee9497024102e7e738
zocomputer <help@zocomputer.com>
2026-06-15 08:46:16 +0000

    Add Astro blog structure and sushi content

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.coerce.date(),
+    description: z.string(),
+  }),
+});
+
+export const collections = { blog };
